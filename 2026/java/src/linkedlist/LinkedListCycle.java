99f8078fe96b8c6bdc35b52c3bc3ad0e24fb114b
Saurabh Somani <somani.saurabh7@gmail.com>
2026-05-25 14:26:11 +0530

    fast & slow pointer, fast & it's next cannot be null for while loop

---
+++ 2026/java/src/linkedlist/LinkedListCycle.java
@@ -0,0 +1,92 @@
+package src.linkedlist;
+
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle in a linked list
+if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos
+is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ */
+
+/*
+Time Complexity: O(N)
+
+Space Complexity: O(1)
+ */
+
+public class LinkedListCycle {
+    private boolean hasCycle(ListNode head){
+        // sanity check
+        if(head == null || head.next == null){
+            return false;
+        }
+
+        // Logic: fast and slow pointer. Termination check: slow == fast. While loop check fast is not null && fast.next
+        // isn't null. Because we cannot invoke '.next' on null but '.next' can be null. Since fast pointer moves by 2
+        // next, i.e, fast.next.next
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+        LinkedListCycle linkedListCycle = new LinkedListCycle();
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(0);
+        ListNode l4 = new ListNode(-4);
+
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l2;
+
+        System.out.println(linkedListCycle.hasCycle(l1));
+
+        ListNode l5 = new ListNode(1);
+        ListNode l6 = new ListNode(2);
+
+        l5.next = l6;
+        l6.next = l5;
+
+        System.out.println(linkedListCycle.hasCycle(l5));
+
+        ListNode l7 = new ListNode(1);
+        System.out.println(linkedListCycle.hasCycle(l7));
+    }
+}
