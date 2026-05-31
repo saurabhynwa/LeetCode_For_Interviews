5785d945136e7ac5bd0dd1ae847efd1e3d603f14
Saurabh Somani <somani.saurabh7@gmail.com>
2026-05-31 16:21:46 +0530

    leetcode hard

--- 2026/java/src/binarySearch/SplitArrayLargestSum.java
+++ 2026/java/src/binarySearch/SplitArrayLargestSum.java
@@ -1,5 +1,7 @@
 package src.binarySearch;
 
+// LeetCode HARD (problem number 410)
+
 /*
 Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any
 subarray is minimized. Return the minimized largest sum of the split. A subarray is a contiguous part of the array.
